044e4f669bb46fdd699eaa13352de8f237475420
replicat0r <replicat0r.misc@gmail.com>
2019-01-28 19:47:54 +0000

    init and added neem

---
+++ neem/mix.exs
@@ -0,0 +1,28 @@
+defmodule Neem.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :neem,
+      version: "0.1.0",
+      elixir: "~> 1.7",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {Neem.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+    {:cowboy, "~> 1.0.0"}
+    ]
+  end
+end
